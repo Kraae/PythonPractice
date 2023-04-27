0ae946b731bac5bf8ce23ec446959f38dcc51a03
Kraae <117115729+Kraae@users.noreply.github.com>
2023-04-27 09:53:24 -0400

    Add files via upload

---
+++ count_up.py
@@ -0,0 +1,35 @@
+def count_up(start, stop):
+    """Print all numbers from start up to and including stop.
+
+    For example:
+
+        count_up(5, 7)
+
+   should print:
+
+        5
+        6
+        7
+    """
+
+    # YOUR CODE HERE
+def counter(start, stop):
+    x = start
+    if x > stop:
+        return_string = "Counting down: "
+        while x >= stop:
+            return_string += str(x)
+            if x > stop:
+                return_string += ","
+            x -= 1 
+    else:
+        return_string = "Counting up: "
+        while x <= stop:
+            return_string += str(x)
+            if x < stop:
+                return_string += ","
+            x +=1
+    return return_string
+
+
+count_up(5, 7)        
